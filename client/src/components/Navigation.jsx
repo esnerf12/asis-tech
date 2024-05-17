4570a49b4e716b7c521d464d7e9eaf364b129140
Anderson Tovar <andersontovar09@gmail.com>
2024-05-17 14:20:56 -0400

    validation currentUser

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -37,12 +37,14 @@ export function Navigation({ currentUser, setCurrentUser }) {
                 </div>
                 <div className="grid grid-cols-2 bg-slate-100 rounded-3xl shadow-xl px-10 py-3">
                     <div className="flex justify-center items-center gap-16">
-                        <div className="flex justify-center items-center gap-1 px-5 py-4 rounded-2xl bg-slate-200">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
-                                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
-                            </svg>
-                            <span className="font-bold">Inicio</span>
-                        </div>
+                        { currentUser && (
+                            <div className="flex justify-center items-center gap-1 px-5 py-4 rounded-2xl bg-slate-200">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
+                                    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+                                </svg>
+                                <span className="font-bold">Inicio</span>
+                            </div>
+                        )}
                         <div className="flex justify-center items-center gap-1 px-5 py-4 rounded-2xl bg-slate-200">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-building-fill-check" viewBox="0 0 16 16">
                                 <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514"/>
@@ -58,17 +60,20 @@ export function Navigation({ currentUser, setCurrentUser }) {
                         </div>
                     </div>
                     <div className="flex justify-end items-center gap-4">
-                        <Link to="/login" className="flex justify-center items-center gap-1 px-5 py-4 rounded-2xl bg-slate-200">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0z"/>
-                                <path fillRule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
-                            </svg>
-                            <span>Ingresar</span>
-                        </Link>
-                        <Link to="/register" className="flex justify-center items-center gap-1 px-5 py-4 rounded-2xl bg-blue-400">
-                            <span>Registrarse</span>
-                        </Link>
-                        { currentUser && (
+                        { !currentUser ? (
+                            <>
+                                <Link to="/login" className="flex justify-center items-center gap-1 px-5 py-4 rounded-2xl bg-slate-200">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0z"/>
+                                        <path fillRule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                                    </svg>
+                                    <span>Ingresar</span>
+                                </Link>
+                                <Link to="/register" className="flex justify-center items-center gap-1 px-5 py-4 rounded-2xl bg-blue-400">
+                                    <span>Registrarse</span>
+                                </Link>
+                            </>
+                        ) : (
                             <form onSubmit={e => submitLogout(e)}>
                                 <button className='flex justify-center items-center gap-1 px-5 py-4 rounded-2xl bg-slate-200'>
                                     <span>Salir</span>
@@ -77,7 +82,7 @@ export function Navigation({ currentUser, setCurrentUser }) {
                                     </svg>
                                 </button>
                             </form>
-                        )}
+                        ) } 
                     </div>
                 </div>
             </nav>
