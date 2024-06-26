aa71aa426860fcccd8b4ad803632d50d92efbea5
Anderson Tovar <andersontovar09@gmail.com>
2024-06-26 18:16:17 -0400

    creating pin funtionality

--- users/models.py
+++ users/models.py
@@ -7,22 +7,27 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 #User
 
 class AppUserManager(BaseUserManager):
-	def create_user(self, email, password=None):
+	def create_user(self, email, pin=None, password=None):
 		if not email:
 			raise ValueError('An email is required.')
 		if not password:
 			raise ValueError('A password is required.')
+		if not pin:
+			raise ValueError('A pin is required.')
 		email = self.normalize_email(email)
 		user = self.model(email=email)
+		user = self.model(pin=pin)
 		user.set_password(password)
 		user.save()
 		return user
-	def create_superuser(self, email, password=None):
+	def create_superuser(self, email, pin=None, password=None):
 		if not email:
 			raise ValueError('An email is required.')
 		if not password:
 			raise ValueError('A password is required.')
-		user = self.create_user(email, password)
+		if not pin:
+			raise ValueError('A pin is required.')
+		user = self.create_user(email, pin, password)
 		user.is_superuser = True
 		user.save()
 		return user
@@ -30,9 +35,10 @@ class AppUserManager(BaseUserManager):
 class AppUser(AbstractBaseUser, PermissionsMixin):
 	user_id = models.AutoField(primary_key=True)
 	email = models.EmailField(max_length=50, unique=True)
-	username = models.CharField(max_length=50)
+	username = models.CharField(max_length=50, unique=True)
+	pin = models.CharField(max_length=6, null=True)
 	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
+	REQUIRED_FIELDS = ['username', 'pin']
 	objects = AppUserManager()
 	def __str__(self):
 		return self.username
--- users/serializer.py
+++ users/serializer.py
@@ -11,6 +11,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def create(self, clean_data):
         user_obj = UserModel.objects.create_user(email=clean_data['email'], password=clean_data['password'])
         user_obj.username = clean_data['username']
+        user_obj.pin = clean_data['pin']
         user_obj.save()
         return user_obj
 
